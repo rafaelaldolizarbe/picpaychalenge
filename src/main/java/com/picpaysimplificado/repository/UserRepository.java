cb048854d23eca595390a9293ebe15e3b49754f2
rafao <rafael.chuchon@sptech.school>
2024-07-21 21:48:09 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimplificado/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado.repository;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserById(Long id);
+    Optional<User> findUserByCpf(String document);
+
+}
