194ec4ee58225eae3228a82ab0b1c7503941fe7b
rafao <rafael.chuchon@sptech.school>
2024-07-23 17:09:20 -0300

    feat: update Notification service

--- src/main/java/com/picpaysimplificado/service/NotificationService.java
+++ src/main/java/com/picpaysimplificado/service/NotificationService.java
@@ -18,12 +18,13 @@ public class NotificationService {
     public void sendNotification(User user , String message)  throws Exception{
         String email = user.getEmail();
         NotificationDto notificationRequest = new NotificationDto(email, message);
-        ResponseEntity<String> notificationResponse= restTemplate.postForEntity("https://util.devi.tools/api/notification", notificationRequest, String.class);
-
-        if (!(notificationResponse.getStatusCode()== HttpStatus.OK)){
-            System.out.println("Erro ao enviar notificação");
-            throw new Exception("Serviço de notificação encontra-se fora do ar");
-
-        }
+//        ResponseEntity<String> notificationResponse= restTemplate.postForEntity("https://util.devi.tools/api/notification", notificationRequest, String.class);
+//
+//        if (!(notificationResponse.getStatusCode()== HttpStatus.OK)){
+//            System.out.println("Erro ao enviar notificação");
+//            throw new Exception("Serviço de notificação encontra-se fora do ar");
+//
+//        }
+        System.out.println("Notificação enviada com sucesso");
     }
 }
